01ca0437581edd09b29a17b7f4c70eb5b30d1f15
Usanim <167047045+Usanim@users.noreply.github.com>
2024-04-17 17:16:26 +0900

    Feat_Life
    3번 연속 실패시 게임 종료되는 코드

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,6 +19,10 @@ public class GameManager : MonoBehaviour
     public int cardCount = 0;
 
     float time = 0.0f;
+    public int DeathCount;
+    public GameObject imageDeath1;
+    public GameObject imageDeath2;
+    public GameObject imageDeath3;
 
     private void Awake()
     {
@@ -41,34 +45,73 @@ public class GameManager : MonoBehaviour
         time += Time.deltaTime;
         timeTxt.text = time.ToString("N2");
 
-        if(time > 30f)
+        if (time > 30f)
         {
             endTxt.SetActive(true);
             Time.timeScale = 0.0f;
         }
-        
+
     }
 
     public void Matched()
     {
-        if(firstCard.idx == secondCard.idx)
+        if (firstCard.idx == secondCard.idx)
         {
             audioSource.PlayOneShot(clip);
             firstCard.DestroyCard();
             secondCard.DestroyCard();
             cardCount -= 2;
-            if(cardCount == 0)
+            if (cardCount == 0)
             {
                 Time.timeScale = 0.0f;
                 endTxt.SetActive(true);
             }
+            else if (cardCount != 0)
+            {
+                if (DeathCount == 1)
+                {
+
+                    firstCard.DestroyCard();
+                    secondCard.DestroyCard();
+                    DeathCount -= 1;
+                    imageDeath1.SetActive(false);
+                }
+                else if (DeathCount == 2)
+                {
+                    firstCard.DestroyCard();
+                    secondCard.DestroyCard();
+                    DeathCount -= 2;
+                    imageDeath1.SetActive(false);
+                    imageDeath2.SetActive(false);
+                }
+            }
+
         }
+
         else
         {
             firstCard.CloseCard();
             secondCard.CloseCard();
+            DeathCount++;
+            if (DeathCount == 1)
+            {
+                imageDeath1.SetActive(true);
+                firstCard = null;
+                secondCard = null;
+            }
+            if (DeathCount == 2)
+            {
+                imageDeath2.SetActive(true);
+                firstCard = null;
+                secondCard = null;
+            }
+            if (DeathCount == 3)
+            {
+                imageDeath3.SetActive(true);
+                Time.timeScale = 0.0f;
+                endTxt.SetActive(true);
+            }
         }
-
         firstCard = null;
         secondCard = null;
     }
